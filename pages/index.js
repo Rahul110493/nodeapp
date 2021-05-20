d43da8a86dbe6a14072497ccac23da1df4e1bfee
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-05-20 17:18:29 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,9 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">My Node.js app V1</a>
+          Welcome to <a href="https://nextjs.org">Next Node.js Page</a>
         </h1>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
