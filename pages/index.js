1e65d7441b5f15980da5680bdc4239c13d86c2e6
Rahul110493 <38890875+Rahul110493@users.noreply.github.com>
2021-05-21 13:34:43 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Nodejs App</a>
+          Welcome to <a href="https://nextjs.org"> App</a>
         </h1>
 
 
